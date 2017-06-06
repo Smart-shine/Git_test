59226585b4a056d0ede77ed654f4f3382fb72c59
Smart-shine <1542962286@qq.com>
2017-06-06 10:47:48 +0800

    Update MainActivity.java
    制造冲突

--- app/src/main/java/com/example/niuanqi/git_test/MainActivity.java
+++ app/src/main/java/com/example/niuanqi/git_test/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity);
         Log.d("String","niuanqi");
         Log.d("","牛安琪");
     }
